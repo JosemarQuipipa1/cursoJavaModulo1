2018a231ca12deff5ae0edc532846b1be43dc00d
JosemarQuipipa <86106628+JosemarQuipipa@users.noreply.github.com>
2023-06-30 10:21:34 +0100

    commit 30.06.2023

---
+++ src/com/quipipa/cursojava/ExercicioDaaula16_17/Exercicio11.java
@@ -0,0 +1,26 @@
+package com.quipipa.cursojava.ExercicioDaaula16_17;
+
+import java.util.Scanner;
+
+public class Exercicio11 {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+
+		int soma = 0;
+		System.out.println("Informa o primeiro numero");
+		int num1 = scan.nextInt();
+
+		System.out.println("Informa o segundo numero");
+		int num2 = scan.nextInt();
+		
+		for(int i = num1; i < num2; i++) {
+			
+			System.out.println(i);
+			soma = soma + i; 
+		}
+
+		System.out.println(" A soma dos numero: "+soma);
+	}
+
+}
