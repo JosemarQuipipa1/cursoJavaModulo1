d48dea2268ad97dbfc965f6a9bf78f0c72cd4cd2
JosemarQuipipa <86106628+JosemarQuipipa@users.noreply.github.com>
2023-07-21 11:22:34 +0100

    21.07.2023

---
+++ src/com/quipipa/cursojava/aula34/Main.java
@@ -0,0 +1,15 @@
+package com.quipipa.cursojava.aula34;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		//MinhaCalculadora calculo = new MinhaCalculadora();
+		
+		MinhaCalculadora.soma(16, 50);
+		
+		// o metodo static torna a instância redondante ex: MinhaCalculadora calculo = new MinhaCalculadora();
+		// já não será necessario declarar
+	}
+
+}
